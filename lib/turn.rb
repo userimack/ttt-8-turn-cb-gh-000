5bdd0111273ea112aad0d7657c572190c4bab7d3
Mahendra Yadav <mahendra.k12@gmail.com>
2018-04-23 10:46:58 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,19 @@
+def display_board(board)
+  puts board
+end
+
+def valid_move?(board, index)
+  if position_taken?(board, index) || index < 0 || index > 8
+    false 
+  else 
+    true 
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil 
+    false 
+  else 
+    true 
+  end 
+end
